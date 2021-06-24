f6b5d3351927d372b75fe501eb5394e1eb4eee2d
Tyler Young <tyler@x-plane.com>
2021-06-24 06:52:51 -0500

    Tests fail intermittently because of shared dependency on global UserCounter

--- test/user_counter_test.exs
+++ test/user_counter_test.exs
@@ -1,8 +1,20 @@
 defmodule UserCounterTest do
   use ExUnit.Case, async: true
 
+  # TESTS FAIL!
+  # Some runs, "initializes to zero" goes first and everything
+  # passes. Other runs, "increment" goes first, so "initializes"
+  # sees a count of 2.
+
   test "initializes to zero" do
-    # Woohoo, it works!
     assert UserCounter.count() == 0
+  end
+
+  test "increments" do
+    assert UserCounter.increment()
+    assert UserCounter.count() == 1
+
+    assert UserCounter.increment()
+    assert UserCounter.count() == 2
   end
 end
